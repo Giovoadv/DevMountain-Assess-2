02f72309877d2c5294540e44c672d371f7c306dc
Giovanni Decenvirale <giovo_adv@hotmail.com>
2023-08-18 15:35:30 -0500

    first commit

---
+++ javascript/text.js
@@ -0,0 +1,9 @@
+function getZ() {
+    const nestedLetters = ['m', 'g', 'e', 'q', 'h', ['n', 'b', ['v', 'z', 'y', 'r']], 'a'];
+    for(let i = 0; i < nestedLetters.length; i++){
+      if(nestedLetters[i] === 'z'){
+  
+      }
+    }
+    console.log(nestedLetters);
+}
